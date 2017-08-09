d1e51285fabb686f9c48e90cce3c70949b81065d
Steve Allred <steven.j.allred@gmail.com>
2017-08-08 20:17:57 -0600

    Configure Basic Security

--- back/src/main/java/io/xpk/web/UserController.java
+++ back/src/main/java/io/xpk/web/UserController.java
@@ -3,10 +3,10 @@ package io.xpk.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
 public class UserController {
 
-  @RequestMapping
+  @RequestMapping("/hello")
   public String temp() {
     return "hello world!";
   }
